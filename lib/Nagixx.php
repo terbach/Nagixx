4011d9b4b9ef1472744c7fd6429f2dcef13b815c
Timo Erbach <terbach@netbixx.com>
2017-01-14 21:51:58 +0100

    Updated correct version number.

--- lib/Nagixx.php
+++ lib/Nagixx.php
@@ -22,7 +22,7 @@ class Nagixx
     /**
      * The actual version of the Nagixx.
      */
-    const VERSION = '1.1.3';
+    const VERSION = '1.1.5';
 
     /**
      * The concrete plugin to make the check.
